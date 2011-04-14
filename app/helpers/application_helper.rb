351bc02ccc3089fb71e1d27706f872640f1379cc
Cody Krieger <cody.krieger@gmail.com>
2011-04-14 08:39:45 -0400

    started moving some content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,24 @@
 module ApplicationHelper
   
+  # head and meta
+  
+  def title(t1, *args)
+    content_for :title do
+      "<h1 id='title'>#{t1}</h1>".html_safe unless t1.to_s.empty?
+    end
+    content_for :title_tag do
+      "#{args[0] || t1} - "
+    end
+  end
+  
+  def metadesc(text)
+    unless text.to_s.empty?
+      content_for :meta_desc do
+        text
+      end
+    end
+  end
+  
   def stylesheets(*args)
     args.each do |arg|
       content_for :stylesheets do

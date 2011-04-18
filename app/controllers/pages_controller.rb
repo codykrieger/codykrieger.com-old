f756801d1b51bf42a38befc4aa162bda26fe44fb
Cody Krieger <cody.krieger@gmail.com>
2011-04-17 21:58:52 -0400

    comments for the pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   rescue_from ActionView::MissingTemplate do |exception|
     if exception.message =~ %r{Missing template pages/}
       # raise ActionController::RoutingError, "No such page: #{params[:slug]}"
+      
+      # render the 404 page if we still haven't found what we're looking for
       render '/public/404.html', :layout => nil, :status => 404
     else
       raise exception
@@ -11,8 +13,11 @@ class PagesController < ApplicationController
 
   def dynamic_page
     begin
+      # try to render the page as-is
       render :template => current_page
     rescue
+      # try to render the slug/index if the page we're looking for
+      # is the index of a folder
       render :template => "#{current_page}/index"
     end
   end
@@ -20,8 +25,7 @@ class PagesController < ApplicationController
 protected
 
   def current_page
-    template_path = params[:slug].to_s.downcase
-    "pages/#{template_path}"
+    "pages/#{params[:slug].to_s.downcase}"
   end
   
 end

9357339c63c2a2814461081305557d37063d2d34
Cody Krieger <cody.krieger@gmail.com>
2011-04-17 22:01:04 -0400

    added some redirect routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 CodykriegerCom::Application.routes.draw do
-  root :to => 'home#index'
-  
   match 'admin' => 'admin/dashboard#index'
   
   devise_for :users
   namespace "admin" do
     resources :users
   end
+  
+  match '/gfxCardStatus/*file.zip' => redirect("/downloads/%{file}.zip")
+  match '/downloads(/*file)' => 'downloads#download_file'
+  
+  match '/compressify/support.html' => redirect("/compressify/support")
+  
+  match '/getintouch' => redirect("/contact")
+  match '/work' => redirect("/about/work")
+  match '/blog' => redirect("http://blog.codykrieger.com")
+  match '/blog/*path' => redirect("http://blog.codykrieger.com/%{path}")
+  
+  root :to => 'home#index'
   
   match '/*slug' => 'pages#dynamic_page', :constraints => { :fullpath => /^\/(?!assets).+/ }
 end
